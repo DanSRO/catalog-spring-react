06462cc7f7b8b9767bc1955015935bf26f8701c8
daniel dantas <Daniel Dantas>
2024-08-29 11:42:52 -0300

    DTO

--- backend/src/main/java/com/catalog/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/catalog/catalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.catalog.catalog.resources;
 
+import com.catalog.catalog.dto.CategoryDTO;
 import com.catalog.catalog.entities.Category;
 import com.catalog.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- backend/src/main/java/com/catalog/catalog/services/CategoryService.java
+++ backend/src/main/java/com/catalog/catalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.catalog.catalog.services;
 
+import com.catalog.catalog.dto.CategoryDTO;
 import com.catalog.catalog.entities.Category;
 import com.catalog.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +18,16 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        /*
+        List<Category> list = repository.findAll();
+        List<CategoryDTO> listDTO = new ArrayList<>();
+        for(Category cat : list){
+            listDTO.add(new CategoryDTO(cat));
+        }
+        return listDTO;
+         */
+        List<Category> list = repository.findAll();
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }

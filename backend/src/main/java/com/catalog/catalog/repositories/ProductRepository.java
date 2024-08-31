3753df65d8b498de8ee00faa10bf7386d3612ea4
daniel dantas <Daniel Dantas>
2024-08-31 19:42:59 -0300

    Product resource, service, repository

---
+++ backend/src/main/java/com/catalog/catalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.catalog.catalog.repositories;
+
+import com.catalog.catalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
